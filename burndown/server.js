e9f2907192a86083d982e7f79ff04af02f225996
Pavlo <wupa4ok@gmail.com>
2018-05-27 22:49:18 -0400

    Deploy to Heroku

--- burndown/server.js
+++ burndown/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/burndown'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
